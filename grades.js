6eba2fad6d9f9cde72f0102b0f0ab789a75796c7
Haden Hudson <mccoy271@gmail.com>
2018-02-01 15:27:44 -0600

    finished

--- grades.js
+++ grades.js
@@ -5,13 +5,13 @@ for ( let i = 0; i < scores.length; i++){
     var grade = scores[i];
 
     switch(true){
-        case(scores[i] >= 90):
+        case(scores[i] >= 91):
         grades["A"] +=1;
         break;
-        case(scores[i] >= 80):
+        case(scores[i] >= 81):
         grades["B"] +=1;
         break;
-        case(scores[i] >= 70):
+        case(scores[i] >= 71):
         grades["C"] +=1;
         break;
         case(scores[i] >= 61):
@@ -36,9 +36,26 @@ scores.sort(function(a, b){return b - a});
 console.log("Highest Score = ", scores[0]);
 
 /* Most Common Grade */
+var currentGradeCount = 0;
+var highestGrade = [];
+var newGradeCount = 100;
+var leastFrequent = [];
 
 
-
-
-
+for (var x in grades) {
+   if(grades[x] > currentGradeCount) {
+       highestGrade = [x];
+       currentGradeCount = grades[x];
+   } else if(grades[x] === currentGradeCount) {
+        highestGrade.push(x);
+   }
+   if(grades[x] < newGradeCount){
+       leastFrequent = [x];
+       newGradeCount = grades[x];;
+   } else if(grades[x] === newGradeCount){
+       leastFrequent.push(x);
+   }
+}
+console.log("The most frequent grade is ", highestGrade);
+console.log("The least frequent grade is ", leastFrequent);
 
